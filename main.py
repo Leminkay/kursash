79a3cf0b58a560ebf243f704c5c70412085a5613
Leminkay <Leminkay@gmail.com>
2019-12-21 16:11:36 +0500

    speed_test()

--- main.py
+++ main.py
@@ -4,101 +4,49 @@ import comfunc
 import time
 import rsa # external lib 
 
+def speed_test():
+    f_timer = 0
+    l_timer = 0
+    rm_timer = 0
 
+    t = 100
 
+    for i in range(t):
+        keyf = Key()
+        keyrm = Key()
+        
+        bitl = 512
+        m = "tempmesmesmes"
+        #testinf fermat
+        start_time = time.time()
+        keyf.GenerateKeysFermat(bitl//2)
+        (e, n) = keyf.GetPublicKey()
+        (d,n,q,p) = keyf.GetPrivateKey()
+        num = comfunc.text_to_num(m)
+        c = pow(num, e,n)
+        d = pow(c, d, n)
+        f_timer += time.time() - start_time
+        #test rm
+        start_time = time.time()
+        keyrm.GenerateKeysRM(bitl//2)
+        (e, n) = keyrm.GetPublicKey()
+        (d,n,q,p) = keyrm.GetPrivateKey()
+        num = comfunc.text_to_num(m)
+        c = pow(num, e,n)
+        d = pow(c, d, n)
+        rm_timer += time.time() - start_time
+        #test lib
+        start_time = time.time()
+        (pub, priv) = rsa.newkeys(bitl)
+        ms = m.encode("utf8")
+        c = rsa.encrypt(ms, pub)
+        d = rsa.decrypt(c, priv)
+        ms = ms.decode("utf8")
+        l_timer += time.time() - start_time
 
+    print("512 bit len crypt encrypt time:")
+    print("for fermat test: ", f_timer / t)
+    print("for frm test: ", rm_timer / t)
+    print("for rsa lib test: ", l_timer / t) 
 
-
-
-
-ferm = 0
-lib = 0
-rm = 0
-
-
-
-for i in range(10000):
-    a= random.randint(2**512,2**513)
-    start_time = time.time()
-    t1  = comfunc.ftest(a)
-    ferm +=time.time() - start_time
-    start_time = time.time()
-    t2 = comfunc.rmtest(a)
-    rm = time.time() - start_time
-    if t1 != t2:
-        print(a)
-
-print(ferm/100, rm /100)
-
-
-print("Input key length")
-x = int(input())
- 
-
-key = Key()
-start_time = time.time()
-key.GenerateKeysFermat(x//2)
-#(e, n) = key.GetPublicKey()
-#(d, n, q, p) = key.GetPrivateKey()
-#print("fermat")
-#print("time passed: ------", time.time() - start_time)
-ferm += time.time() - start_time
-print(ferm)
-#print('Your Public Key is ', e , n, sep =' ')
-
-start_time = time.time()
-#print(rm test)
-key.GenerateKeysRM(x//2)
-rm += time.time() - start_time
-#print("rsa lib")
-print(rm)
-
-
-start_time = time.time()
-
-(pub, priv) = rsa.newkeys(x)
-
-   # print("time passed: ------", time.time() - start_time)
-lib += time.time() - start_time
-print(lib)
-
-
-print('Input message to encrypt')
-m = input()
-
-
-(e, n) = key.GetPublicKey()
-(d,n,q,p) = key.GetPrivateKey()
-num = comfunc.text_to_num(m)
-randc = 4;
-num = comfunc.add_rand_bits(num, randc)
-
-#encrypting
-c = pow(num, e,n)
-
-
-#decrypting
-d = pow(c, d, n)
-
-#print('Encrypted message:')
-#print(c)
-
-d = comfunc.remove_rand_bits(d, 4)
-
-s = comfunc.num_to_text(d)
-
-
-#print('Decrypted message')
-#print(s)
-
-
-mr = m.encode('utf8')
-
-
-
-crypt = rsa.encrypt(mr, pub)
-
-
-
-dec = rsa.decrypt(crypt, priv)
-
+speed_test()
